29cad3b28802a992053f58902a38f2b898f447de
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-05-01 19:16:44 +0900

    classdiagram

--- src/main/java/com/hlkw/cafe/service/CommentService.java
+++ src/main/java/com/hlkw/cafe/service/CommentService.java
@@ -1,9 +1,12 @@
 package com.hlkw.cafe.service;
 
+import com.hlkw.cafe.repository.CommentMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-
+@RequiredArgsConstructor
 public class CommentService {
 
+    private final CommentMapper mapper;
 }
--- src/main/java/com/hlkw/cafe/service/MemberService.java
+++ src/main/java/com/hlkw/cafe/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.hlkw.cafe.service;
 
+import com.hlkw.cafe.repository.MemberMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberMapper mapper;
 
 }
