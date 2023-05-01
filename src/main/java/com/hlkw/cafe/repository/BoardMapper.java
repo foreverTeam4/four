ac04e00184a3310174ef6e5679d47e2f71ee031e
myblog0419 <myblog0419@naver.com>
2023-05-01 09:03:29 +0900

    "게시판 기본세팅 재설정 commit"

--- src/main/java/com/hlkw/cafe/repository/BoardMapper.java
+++ src/main/java/com/hlkw/cafe/repository/BoardMapper.java
@@ -1,16 +1,10 @@
 package com.hlkw.cafe.repository;
 
-import com.hlkw.cafe.dto.writeDto;
-import com.hlkw.cafe.entity.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface BoardMapper {
 
-    public boolean write(writeDto dto);
-
-    public Board findOne(int boardNo);
-    public boolean remove(int boardNo);
 
 
 
--- src/main/java/com/hlkw/cafe/repository/CommentMapper.java
+++ src/main/java/com/hlkw/cafe/repository/CommentMapper.java
@@ -1,15 +1,9 @@
 package com.hlkw.cafe.repository;
 
-import com.hlkw.cafe.dto.addCommentDto;
-import com.hlkw.cafe.entity.Comment;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CommentMapper {
 
-    public boolean save(Comment comment);
-
-    public boolean modify(addCommentDto dto);
-    public boolean remove(Comment comment);
 
 }
