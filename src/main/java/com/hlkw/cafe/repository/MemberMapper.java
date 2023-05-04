35c5e6a2fd0e00ad6a635f5aeea140951132adba
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-05-04 21:28:07 +0900

    member save method

--- src/main/java/com/hlkw/cafe/repository/MemberMapper.java
+++ src/main/java/com/hlkw/cafe/repository/MemberMapper.java
@@ -6,8 +6,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
 
-    Member findOne(String id, String pw);
+    void save(Member mbr);
 
+    Member findOne(String id, String pw);
 
 //동우 마이페이지 업데이트
     Member mypageUpdate(Member member);
